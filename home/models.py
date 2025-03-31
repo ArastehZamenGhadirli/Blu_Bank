21ef282bb22c1e3c0d3d2f970064d7b4f791fae4
Arasteh Zamen Ghadirli <41484641+ArastehZz@users.noreply.github.com>
2025-04-01 00:55:25 +0330

    models are added

---
+++ home/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=10,unique=True)
+    password = models.CharField(max_length=20)
+    national_id = models.CharField(max_length=11, unique=True)
+    
+
+class Account(models.Model):
+    name = models.CharField(max_length=100)
+    user =  models.OneToOneField(User , on_delete=models.CASCADE)
+    card_number = models.CharField(max_length=20 , unique=True)
+    phone_number = models.CharField(max_length=10)
+
+class Profit(models.Model):
+    date = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=20 , unique=True)
+    duration = models.IntegerField()
+    account = models.ForeignKey(to=Account,on_delete=models.CASCADE,related_name="Account_Profit")
+
+
+
+
+
+
+
+
+
+    
+    
+
+
+        
+    
+
+
